66906e5725e1f0a90770f7a61149caaf6c9a34da
felipe <srfelipeferreira1904@gmail.com>
2025-12-08 22:18:16 -0300

    comit fault

---
+++ avaliacao.js
@@ -0,0 +1,47 @@
+//opç-01
+
+let = "media de peso"
+
+const peso1 = Number(prompt("Digite o primeiro peso: "))
+const peso2 = Number(prompt("Digite o segundo peso: "))
+const peso3 = Number(prompt("Digite o terceiro peso: "))
+const peso4 = Number(prompt("Digite o Quarto peso: "))
+const peso5 = Number(prompt("Digite o Quinto peso: "))
+
+
+
+const media = (peso1 + peso2 + peso3 + peso4 + peso5) / 5
+
+alert("media de peso: " + media + "")
+
+
+
+//opç-02
+
+
+const numero1 = Number(prompt("Digite o primeiro número: "))
+
+const numero2 = Number(prompt("Digite o segundo número: "))
+
+
+
+alert("Resultado de " + numero1 + "  " + numero2 + " = " + (numero1 + numero2))
+
+//opç-3
+
+const numero1 = Number(prompt("Digite o 1 número: "))
+const numero1 = Number(prompt("Digite o 2 número: "))
+const numero1 = Number(prompt("Digite o 3 número: "))
+const numero1 = Number(prompt("Digite o 4 número: "))
+const numero1 = Number(prompt("Digite o 5 número: "))
+const numero1 = Number(prompt("Digite o 6 número: "))
+const numero1 = Number(prompt("Digite o 7 número: "))
+const numero1 = Number(prompt("Digite o 8 número: "))
+const numero1 = Number(prompt("Digite o 9 número: "))
+const numero1 = Number(prompt("Digite o 10 número: "))
+
+
+const resto = numero % 2
+
+if (resto == 0) {
+}
